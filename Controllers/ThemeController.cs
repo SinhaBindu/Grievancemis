91d0686924e8226a61a8b22da8309ed5066a695a
hussainkor <hussainkor@gmail.com>
2024-10-20 22:08:39 +0530

    Added dashboard theme

---
+++ Controllers/ThemeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grievancemis.Controllers
+{
+    public class ThemeController : Controller
+    {
+        // GET: Theme
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        public ActionResult FormBasic()
+        {
+            return View();
+        }
+
+        public ActionResult FormBasics()
+        {
+            return View();
+        }
+
+        public ActionResult Finance()
+        {
+            return View();
+        }
+
+        public ActionResult Datatable()
+        {
+            return View();
+        }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            return View();
+        }
+    }
+}

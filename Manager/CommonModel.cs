bdc716e73e8c06b9635e249209a8acc39a3c9eed
Bindu Kumari <142012740+SinhaBindu@users.noreply.github.com>
2024-12-15 22:57:39 +0530

    committed

--- Manager/CommonModel.cs
+++ Manager/CommonModel.cs
@@ -419,7 +419,7 @@ namespace Grievancemis.Manager
             Grievance_DBEntities _db = new Grievance_DBEntities();
             //int noofsend = 0;
             string To = "", Subject = "", Body = "", ReceiverName = "Hi", maxID = "", RandomValue = "", OTPCode = "";
-            string OtherEmailID = "sinhabinduk@gmail.com,,sinhaharshit829@gmail.com";
+            string OtherEmailID = "sinhabinduk@gmail.com,sinhaharshit829@gmail.com";
             Grievance_DBEntities db_ = new Grievance_DBEntities();
             string bodydata = string.Empty;
             string bodyTemplate = string.Empty;
@@ -514,7 +514,7 @@ namespace Grievancemis.Manager
             }
             catch (Exception ex)
             {
-                CommonModel.ExpSubmit("Tbl_LoginVerification" + tbl_v.Issent, "Grievnce", "SendMailForUser_OTP", "SendMailForUser_commonmodel", ex.Message);
+                CommonModel.ExpSubmit("Tbl_LoginVerification" , "Grievnce", "SendMailForUser_OTP", "SendMailForUser_commonmodel", ex.Message+ "_"+Toemailid);
                 return 0;
 
             }
@@ -588,7 +588,7 @@ namespace Grievancemis.Manager
             }
             catch (Exception ex)
             {
-                CommonModel.ExpSubmit("Tbl_Grievance", "Grievnce", "PanalMailTemplate", "SendSucessfullMailForUserTeam_commonmodel", ex.Message);
+                CommonModel.ExpSubmit("Tbl_Grievance", "Grievnce", "PanalMailTemplate", "SendSucessfullMailForUserTeam_commonmodel", ex.Message + "_" + Toemailid);
                 return 0;
             }
         }
@@ -650,7 +650,7 @@ namespace Grievancemis.Manager
             }
             catch (Exception ex)
             {
-                CommonModel.ExpSubmit("Tbl_Grievance", "Grievnce", "UserTemplateMail", "SendMailPartUser_commonmodel", ex.Message);
+                CommonModel.ExpSubmit("Tbl_Grievance", "Grievnce", "UserTemplateMail", "SendMailPartUser_commonmodel", ex.Message + "_" + Toemailid);
                 return 0;
             }
         }
@@ -720,7 +720,7 @@ namespace Grievancemis.Manager
             }
             catch (Exception ex)
             {
-                CommonModel.ExpSubmit("Tbl_TeamRevertComplain", "Complain_UserCom", "RevartMail", "SendMailRevartPartUser_commonmodel", ex.Message);
+                CommonModel.ExpSubmit("Tbl_TeamRevertComplain", "Complain_UserCom", "RevartMail", "SendMailRevartPartUser_commonmodel", ex.Message + "_" + Toemailid);
                 return 0;
             }
         }
